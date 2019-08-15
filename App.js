c4d0a97bb98da5f513199167f037da56f8ad859e
gudwnsgur <gudwnsgur12@naver.com>
2019-08-15 15:36:59 +0900

    start making timer app for practice React, Redux

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Timer from './components/Timer';
+
+
+export default function App() {
+  return (
+    <Timer />
+  )
+}
